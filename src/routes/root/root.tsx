c3ed844a31958933ecf5ed28c65cd645619329f6
Joonatan Kallio <80262292+JoonatanKallio@users.noreply.github.com>
2024-06-03 21:14:35 +0300

    added radixui (#5)

--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@radix-ui/themes';
+
 export default function Root() {
     return (
         <div>
             <h1>Hello world!</h1>
             <h2>Testing</h2>
+            <Button>Test</Button>
         </div>
     );
 }
